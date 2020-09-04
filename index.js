c77305ff97e9d70ed3a95655fce4720e6950015f
Lauriacunia <Lauriacunia@gmail.com>
2020-09-04 12:11:06 -0300

    Comienzo a codear js de filtros por nombre y rating

---
+++ index.js
@@ -0,0 +1,45 @@
+const filtroNombre = document.querySelector('#input-busqueda');
+const tarjetas = document.getElementsByClassName('tarjeta');
+const filtroRating = document.getElementsByClassName('filtro-estrellas');
+
+// cuando se escriba algo en el input
+filtroNombre.oninput = () => {
+    // recorro una a una cada tarjeta
+    for (let tarjeta of tarjetas) {
+      // me fijo el nombre de la tarjeta y qué buscó el usuario
+      const titulo = tarjeta.dataset.nombre;
+      const busqueda = filtroNombre.value;
+      // si el titulo incluye lo que buscó el usuario...
+      if (titulo.includes(busqueda)) {
+        // le quito la clase "hidden" a la tarjeta
+        tarjeta.classList.remove('hidden');
+      } else {
+        // se la agrego
+        tarjeta.classList.add('hidden');
+      }
+    }
+  };
+  
+  // recorro uno a uno los checkbox
+  for (let checkbox of filtroRating) {
+    // si le hacen clic a uno de los checkbox
+    checkbox.onclick = () => {
+      // recorro una a una las tarjetas
+      for (let tarjeta of tarjetas) {
+        // si el checkbox esta seleccionado
+        if (checkbox.checked) {
+          const rating = tarjeta.dataset.rating;
+          // me fijo si el valor del checkbox es igual al rating de la tarjeta
+          if (checkbox.value === rating) {
+            tarjeta.classList.remove('hidden');
+          } else {
+            tarjeta.classList.add('hidden');
+          }
+          // si el checkbox NO esta seleccionado... 
+        } else {
+          tarjeta.classList.remove('hidden');
+        }
+      }
+    };
+  }
+  
